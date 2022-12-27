7dac1c3e3df4dfdbab2fb5bb408a7c3148ed2c6e
TaeYoung Jin <elisha0103@naver.com>
2022-12-27 13:02:25 +0900

    first commit

---
+++ AppleMarket/AppleMarket/AppleMarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMarketApp.swift
+//  AppleMarket
+//
+//  Created by 진태영 on 2022/12/27.
+//
+
+import SwiftUI
+
+@main
+struct AppleMarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
