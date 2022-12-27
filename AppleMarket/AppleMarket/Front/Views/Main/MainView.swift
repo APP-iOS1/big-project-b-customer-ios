ea66dabaef84e4cfdf6e3fb780ecd83e8d54ee25
MartinLeeSJ <tjr2922@gmail.com>
2022-12-27 16:10:10 +0900

    [Feat] 검색 과정 뷰

--- AppleMarket/AppleMarket/Front/Views/Main/MainView.swift
+++ AppleMarket/AppleMarket/Front/Views/Main/MainView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct MainView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .foregroundColor(.secondary)
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            
     }
 }
 
