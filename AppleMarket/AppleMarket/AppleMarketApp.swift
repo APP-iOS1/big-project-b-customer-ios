e6c7783441dd9c02334c77c57d4112d7886cdac5
zooey <popularyg@gmail.com>
2022-12-28 11:12:14 +0900

    Test: delete testview

--- AppleMarket/AppleMarket/AppleMarketApp.swift
+++ AppleMarket/AppleMarket/AppleMarketApp.swift
@@ -25,7 +25,7 @@ struct AppleMarketApp: App {
     
     var body: some Scene {
         WindowGroup {
-            CartTest()
+            ContentView()
                 .environmentObject(UserInfoStore())
         }
     }
