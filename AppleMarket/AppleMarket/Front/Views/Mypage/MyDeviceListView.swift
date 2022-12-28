bb6c0a31dc8a3ef8f20c1be0855c0ec7f2e5aa63
Jero <62836016+SMizzz@users.noreply.github.com>
2022-12-28 13:17:01 +0900

    [Feat] Address, MyDevice.

--- AppleMarket/AppleMarket/Front/Views/Mypage/MyDeviceListView.swift
+++ AppleMarket/AppleMarket/Front/Views/Mypage/MyDeviceListView.swift
@@ -8,15 +8,61 @@
 import SwiftUI
 
 struct MyDeviceListView: View {
+    @State private var isShowingSheet: Bool = false
+    
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("기기 목록")
-                .font(.title2)
-                .bold()
-            Spacer()
+        NavigationStack {
+            VStack(alignment: .leading) {
+                Text("기기 목록")
+                    .font(.title2)
+                    .bold()
+                Spacer()
+                
+                List {
+                    Section {
+                        ForEach(myDeviceInform, id: \.self) { device in
+                            AsyncImage(url: URL(string: device.imageName)) { image in
+                                HStack(alignment: .center) {
+                                    image
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(width: 60)
+                                    
+                                    VStack(alignment: .leading, spacing: 3) {
+                                        Text(device.deviceName)
+                                            .font(.headline)
+                                        Text(device.deviceModelName)
+                                            .font(.subheadline)
+                                    }
+                                }
+                            } placeholder: {
+                                ProgressView()
+                            }
+                        }
+                        .frame(height: 55)
+                    }
+                    
+                    Button {
+                        isShowingSheet.toggle()
+                    } label: {
+                        HStack {
+                            Image(systemName: "plus")
+                                .foregroundColor(.black)
+                            Text("기기추가하기")
+                                .foregroundColor(.black)
+                        }
+                    }
+                    .frame(height: 40)
+                    .sheet(isPresented: $isShowingSheet) {
+                        AddMyDeviceView(isShowingSheet: $isShowingSheet)
+                            .presentationDetents([.medium,.large])
+                    }
+                }
+            }
+            .padding()
+            .background(Color("myPageBGColor"))
         }
-        .padding()
-        .background(Color("myPageBGColor"))
+        
     }
 }
 
